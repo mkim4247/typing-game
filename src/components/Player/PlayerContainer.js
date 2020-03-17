714943136c750d590a216363e303837f1297a474
Michael Kim <m.kim4247@gmail.com>
2020-03-16 23:53:29 -0400

    latest

--- src/components/Player/PlayerContainer.js
+++ src/components/Player/PlayerContainer.js
@@ -1,10 +1,13 @@
 import React from 'react'
+import PlayerDisplay from './PlayerDisplay'
+import PlayerInput from './PlayerInput'
 
 class PlayerContainer extends React.Component {
   render(){
     return(
       <div>
-
+        <PlayerDisplay />
+        <PlayerInput />
       </div>
     )
   }
